5a7899a9fe12c6818777b6e334b7f81676d9f6b8
M.hammad3373 <alihammadg2003@gmail.com>
2024-08-12 15:39:46 +0500

    next-js-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-0">
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Snapchat-313278848.jpg" alt="hammad" width="350" height="450"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
-        “Tailwind CSS is the only framework that I've seen scale
-        on large teams. It’s easy to customize, adapts to any design,
-        and the build size is tiny.”
+        “As a law student, you're diving into the complexities of legal systems,
+         learning to analyze statutes, case law, and legal principles. 
+         You're developing critical thinking skills,
+          honing your ability to construct well-reasoned arguments,
+           and gaining an understanding of the role of law in society. 
+           Your studies will prepare you for a variety of careers in the legal field,
+            from practicing attorney to policymaker.”
       </p>
     </blockquote>
     <figcaption className="font-medium">
@@ -19,7 +23,7 @@ export default function Home() {
         Hammad Ali
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-        Student of Law, Biggner
+        Student , LAW 
       </div>
     </figcaption>
   </div>

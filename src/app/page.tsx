9a7d776a1ba9952d87597b833561fa740712c956
M.hammad3373 <alihammadg2003@gmail.com>
2024-08-27 17:08:10 +0500

    next-js-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,22 +8,16 @@ export default function Home() {
   <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Snapchat-313278848.jpg" alt="hammad" width="350" height="450"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
-      <p className="text-lg font-medium">
-        “As a law student, you're diving into the complexities of legal systems,
-         learning to analyze statutes, case law, and legal principles. 
-         You're developing critical thinking skills,
-          honing your ability to construct well-reasoned arguments,
-           and gaining an understanding of the role of law in society. 
-           Your studies will prepare you for a variety of careers in the legal field,
-            from practicing attorney to policymaker.”
-      </p>
+      <p className="text-lg font-medium">"Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny." </p>
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-        Hammad Ali
+      Sarah Dayan
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-        Student , LAW 
+      Staff Engineer, Algolia
       </div>
     </figcaption>
   </div>

5fb024d04cc91db69cbf45bb79d1b1ebf148eac8
M.hammad3373 <alihammadg2003@gmail.com>
2024-08-28 18:08:25 +0500

    next-js-1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
         <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-  <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Snapchat-313278848.jpg" alt="hammad" width="350" height="450"/>
+  <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/Snapchat-313278848.jpg" alt="hammad" width="350" height="450"/>
   <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">"Tailwind CSS is the only framework that I've seen scale
@@ -14,10 +14,10 @@ export default function Home() {
     </blockquote>
     <figcaption className="font-medium">
       <div className="text-sky-500 dark:text-sky-400">
-      Sarah Dayan
+      Hammad Ali
       </div>
       <div className="text-slate-700 dark:text-slate-500">
-      Staff Engineer, Algolia
+      Genrative AI, Student
       </div>
     </figcaption>
   </div>
